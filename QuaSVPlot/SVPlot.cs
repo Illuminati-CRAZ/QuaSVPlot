f80fa99c27b67e801c41953e0b81de4f905de442
Illuminati-CRAZ <42056854+Illuminati-CRAZ@users.noreply.github.com>
2021-03-20 14:13:41 -0700

    sure graph displacement as well lol

--- QuaSVPlot/SVPlot.cs
+++ QuaSVPlot/SVPlot.cs
@@ -17,6 +17,8 @@ namespace QuaSVPlot
         
         public LineSeries PositionSeries { get; private set; }
         
+        public LineSeries DisplacementSeries { get; private set; }
+        
         public Qua Map { get; private set; }
         
         public SVPlot()
@@ -36,9 +38,9 @@ namespace QuaSVPlot
             
             // Plot stuff
             Model = new PlotModel { Title = Map.ToString() };
-            Model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Key = "Time" });
-            Model.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Key = "Velocity", Minimum = -10, Maximum = 10});
-            Model.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Key = "Position" });
+            Model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Key = "Time", Title = "Time (ms)" });
+            Model.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Key = "Velocity", Title = "Velocity", Minimum = -10, Maximum = 10});
+            Model.Axes.Add(new LinearAxis { Position = AxisPosition.Right, Key = "Position", Title = "Position", PositionTier = 1 });
             
             // Scroll Velocities
             SVSeries = new StairStepSeries();
@@ -59,7 +61,15 @@ namespace QuaSVPlot
             PositionSeries.DataFieldY = "Position";
             PositionSeries.YAxisKey = "Position";
             
-            // Calculate position data points based off of Quaver's position calculations
+            // Displacement
+            DisplacementSeries = new LineSeries();
+            DisplacementSeries.Title = "Displacement";
+            DisplacementSeries.CanTrackerInterpolatePoints = true;
+            DisplacementSeries.DataFieldX = "Time";
+            DisplacementSeries.DataFieldY = "Displacement";
+            DisplacementSeries.YAxisKey = "Position";
+            
+            // Calculate position and displacement data points based off of Quaver's position calculations
             // This is pretty much just HitObjectManagerKeys.InitializePositionMarkers()
             long position = (long)(Map.SliderVelocities[0].StartTime * Map.InitialScrollVelocity * 100);
             PositionSeries.Points.Add(new DataPoint(Map.SliderVelocities[0].StartTime, position));
@@ -68,10 +78,14 @@ namespace QuaSVPlot
                 position += (long)((Map.SliderVelocities[i].StartTime - Map.SliderVelocities[i - 1].StartTime)
                                     * Map.SliderVelocities[i - 1].Multiplier * 100);
                 PositionSeries.Points.Add(new DataPoint(Map.SliderVelocities[i].StartTime, position));
+                
+                // Displacement function = Position function - Base position function (constant 1x SV)
+                DisplacementSeries.Points.Add(new DataPoint(Map.SliderVelocities[i].StartTime, position - Map.SliderVelocities[i].StartTime * 100));
             }
             
             Model.Series.Add(SVSeries);
             Model.Series.Add(PositionSeries);
+            Model.Series.Add(DisplacementSeries);
         }
     }
 }
